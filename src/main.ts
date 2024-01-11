20da1551fd326eefc5f17d34ae498709cc9a1fa8
isoca7 <isoca7@gmail.com>
2024-01-11 17:33:12 +0100

    message

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,39 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+const redStyle:string="color:red";
+interface Grupo {
+    nombre: string
+    año: number
+    activo: boolean
+    genero: string
+  }
+  
+  const grupoA: Grupo = {
+    nombre: 'The Beatles',
+    año: 1960,
+    activo: true,
+    genero: '🎵 Pop Rock',
+  }
+  
+  const grupoB: Grupo = {
+    nombre: 'Queen',
+    año: 1970,
+    activo: false,
+    genero: '🎸 Rock',
+  }
+  
+  const grupoC: Grupo = {
+    nombre: 'AC DC',
+    año: 1973,
+    activo: true,
+    genero: '🤘 Hard Rock',
+  }
+  
+  const grupoD: Grupo = {
+    nombre: '%cLudwig van Beethoven',
+    año: 1770,
+    activo: false,
+    genero: '🎼 Clásica'
+  }
+  console.log(grupoD.nombre, grupoD.año, grupoD.activo, `%c${grupoD.genero}`, "color: #999")
+  
