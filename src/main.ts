bb71ff82fb1c1fa49b30093f14ae3ce037030740
isoca7 <isoca7@gmail.com>
2024-01-11 19:03:42 +0100

    subida sandbox

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,51 @@
-import "./style.css";
-
-const redStyle:string="color:red";
 interface Grupo {
-    nombre: string
-    año: number
-    activo: boolean
-    genero: string
-  }
-  
-  const grupoA: Grupo = {
-    nombre: 'The Beatles',
-    año: 1960,
-    activo: true,
-    genero: '🎵 Pop Rock',
-  }
-  
-  const grupoB: Grupo = {
-    nombre: 'Queen',
-    año: 1970,
-    activo: false,
-    genero: '🎸 Rock',
-  }
-  
-  const grupoC: Grupo = {
-    nombre: 'AC DC',
-    año: 1973,
-    activo: true,
-    genero: '🤘 Hard Rock',
-  }
-  
-  const grupoD: Grupo = {
-    nombre: '%cLudwig van Beethoven',
-    año: 1770,
-    activo: false,
-    genero: '🎼 Clásica'
-  }
-  console.log(grupoD.nombre, grupoD.año, grupoD.activo, `%c${grupoD.genero}`, "color: #999")
-  
+  nombre: string
+  año: number
+  activo: boolean
+  genero_musical: string
+}
+
+const beatles = {
+  nombre: 'The Beatles',
+  año: 1960,
+  activo: true,
+  genero_musical: '🎵 Pop Rock',
+}
+
+const queen = {
+  nombre: 'Queen',
+  año: 1970,
+  activo: false,
+  genero_musical: '🎸Rock',
+}
+
+const acdc = {
+  nombre: 'AC DC',
+  año: 1973,
+  activo: true,
+  genero_musical: '🤘 Hard Rock',
+}
+const beethoven = {
+  nombre: 'Ludwig van Beethoven',
+  año: 1770,
+  activo: false,
+  genero_musical: '🎼 Clásica',
+}
+const rolling = {
+  nombre: 'The Rolling Stones ',
+  año: 1962,
+  activo: true,
+  genero_musical: '🎸Rock',
+}
+
+function imprimirGrupo(grupo : Grupo) {
+  const estiloTitulo = 'color:green; font-weight:bold;'
+  console.log(`%c${grupo.nombre}`, estiloTitulo)
+  console.log(grupo.año, grupo.activo, grupo.genero_musical)
+}
+
+imprimirGrupo(beatles)
+imprimirGrupo(queen)
+imprimirGrupo(acdc)
+imprimirGrupo(beethoven)
+imprimirGrupo(rolling)
